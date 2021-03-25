66b3b3d374524b39780dc6c4c4f7470a85f79e4c
Russ Taylor <russ@reliasolve.com>
2021-03-25 17:28:54 -0400

    Partial (not yet compiling) implementation of atom-vs.-neighbors scoring

--- Scoring.cpp
+++ Scoring.cpp
@@ -31,11 +31,178 @@ ContactResult closest_contact(Point dot, Point atom, double atom_radius)
   return ret;
 }
 
+int atom_charge(iotbx::pdb::hierarchy::atom const& atom)
+{
+  // Get the tidy version of the charge string stripped of any extra characters.
+  // Detangle the fact that the string claims to have the potential to be optional.
+  // This will make the string either blank or consisting of two characters, [12][+-],
+  // with the first indicating the magnitude and the second indicating the sign.
+  int ret = 0;
+  std::string chStr;
+  auto opt = atom.charge_tidy(true);
+  if (opt) { chStr = opt.get(); }
+
+  if (chStr.size() > 0) {
+    ret = chStr[0] - '0';
+    if (chStr[1] == '-') { ret *= -1; }
+  }
+
+  return ret;
+}
+
 AtomVsAtomDotScorer::ScoreDotsResult AtomVsAtomDotScorer::score_dots(
-  iotbx::pdb::hierarchy::atom sourceAtom, iotbx::pdb::hierarchy::atom targetAtom,
+  iotbx::pdb::hierarchy::atom sourceAtom, double minOccupancy,
+  SpatialQuery spatialQuery, double nearbyRadius, double probeRadius,
+  std::vector<iotbx::pdb::hierarchy::atom> exclude,
   std::vector<Point> dots, double density, bool onlyBumps)
 {
+  // This method is based on AtomPositions::atomScore() from Reduce.
+  // It is passed only the dots that it should score rather than excluding them
+  // internally like that function does.
 
+  // Default return has 0 value for all subscores.
+  ScoreDotsResult ret;
+
+  // Check for invalid parameters
+  if (density <= 0) {
+    return ret;
+  }
+
+  // Make sure that the occupancy of the atom is high enough to score it.
+  if (std::abs(sourceAtom.data->occ) < minOccupancy) {
+    return ret;
+  }
+  /// @todo Check the conformation to make sure this atom is visible in it
+
+  // Find the neighboring atoms that are potentially interacting.
+  // The nonzero minimum distance prevents us from selecting the source atom.
+  std::vector<iotbx::pdb::hierarchy::atom> neighbors = spatialQuery.neighbors(sourceAtom.data->xyz, 0.001, nearbyRadius);
+
+  // Select only those atoms actually interacting: that have sufficient occupancy, for whom the
+  // gap between the Van Der Waals surfaces is less than the probe radius, and which
+  // are not in the excluded list.
+  /// @todo Check the conformation to make sure they are both visible to each other.
+  std::vector<iotbx::pdb::hierarchy::atom> interacting;
+  unsigned sourceID = sourceAtom.data->i_seq;
+  ExtraAtomInfo const& sourceExtra = m_extraInfo[sourceID];
+  for (iotbx::pdb::hierarchy::atom const &a : neighbors) {
+    unsigned aID = a.data->i_seq;
+    if ((sourceID >= m_extraInfo.size()) || (aID >= m_extraInfo.size())) {
+      return ret;
+    }
+    ExtraAtomInfo const& aExtra = m_extraInfo[aID];
+    double nonBondedDistance = sourceExtra.vdwRadius + aExtra.vdwRadius;
+    if ((std::abs(a.data->occ) >= minOccupancy)
+          && ((a.data->xyz - sourceAtom.data->xyz).length() <= nonBondedDistance + probeRadius)
+      @todo replace this find with a scan looking for common data pointer get() outputs due to no == being defined
+          && (std::find(exclude.begin(), exclude.end(), a) == exclude.end())
+        ) {
+      interacting.push_back(a);
+    }
+  }
+
+  /// @todo Useful tidbits.  Remove when done.
+  bool h = sourceAtom.element_is_hydrogen();
+  /// @todo end useful tidbits
+
+  // Run through all of the dots and determine whether and how to score each.
+  for (Point const& d : dots) {
+    // Find the world-space location of the dot by adding it to the location of the source atom.
+    // The probe location is in the same direction as d from the source but is further away by the
+    // probe radius.
+    Point q = sourceAtom.data->xyz + d;
+    Point probLoc = sourceAtom.data->xyz + d.normalize() * (d.length() + probeRadius);
+
+    double minGap = 1e10;                 ///< Nearest atom that we found
+    bool isHydrogenBond = false;          ///< Are we looking at a hydrogen bond to our neighbor?
+    bool tooCloseHydrogenBond = false;    ///< Are we too close to be a hydrogen bond?
+    double hydgrogenBondMinDist = 1e10;   ///< Nearest hydrogen bond we found
+    bool keepDot = false;                 ///< Did we find a neighbor and we're not in a bonded atom?
+
+    iotbx::pdb::hierarchy::atom const *cause = nullptr;
+
+    // Look through each atom to find the one with the smallest gap, which is the one that
+    // the dot would interact with.
+    for (iotbx::pdb::hierarchy::atom const& b : interacting) {
+      ExtraAtomInfo const& bExtra = m_extraInfo[b.data->i_seq];
+      Point locb = b.data->xyz;
+      double vdwb = bExtra.vdwRadius;
+
+      // See if we are too far away to interact, bail if so.
+      double squareDist = (probLoc - locb).length_sq();
+      double pRadPlusVdwb = vdwb + probeRadius;
+      if (squareDist > pRadPlusVdwb * pRadPlusVdwb) {
+        continue;
+      }
+
+      // See if we are within the probe radius past the edge.  If so, we're in contention
+      // to be the nearest atom.
+      double dist = sqrt(squareDist);
+      double probeGap = dist - pRadPlusVdwb;
+      double gap = dist - vdwb;
+      if (probeGap < 0) {
+
+        // See if we replace the currently-closest atom.
+        if (gap < minGap) {
+
+          // Figure out what kind of interaction this is based on the atom types and
+          // charge status of the two atoms.
+          int chargeSource = atom_charge(sourceAtom);
+          int chargeB = atom_charge(b);
+
+          bool bothCharged = (chargeSource != 0) && (chargeB != 0);
+          bool chargeComplement = bothCharged && (chargeSource * chargeB < 0);
+
+          // See if one of the atoms is a hydrogen donor and the other can accept hydrogen bonds.
+          bool couldHBond = (sourceExtra.isDonor && bExtra.isAcceptor)
+            || (sourceExtra.isAcceptor && bExtra.isDonor);
+          if (couldHBond && ((!bothCharged) || chargeComplement)) {
+            isHydrogenBond = true;
+            hydgrogenBondMinDist = bothCharged ? m_minChargedHydrogenBondGap : m_minRegularHydrogenBondGap;
+            tooCloseHydrogenBond = (gap < -hydgrogenBondMinDist);
+          } else {
+            // If this is a dummy hydrogen, then we skip it, it can only be a hydrogen-bond partner
+            if (bExtra.isDummyHydrogen) { continue; }
+            // This is not a hydrogen bond.
+            isHydrogenBond = tooCloseHydrogenBond = false;
+          }
+
+          // Record which atom is the closest and mark the dot to be kept because we found an
+          // atom that is close enough.
+          cause = &b;
+          keepDot = true;
+          minGap = gap;
+        }
+      }
+    }
+
+    // If the dot was close enough to some non-bonded atom, check to see if it should be removed
+    // from consideration because it is also inside an excluded atom.
+    if (keepDot) {
+      /// @todo Check to make sure they are in interacting alternate conformations.
+      for (iotbx::pdb::hierarchy::atom const& e : exclude) {
+        double vdwe = m_extraInfo[e.data->i_seq].vdwRadius;
+        if ((q - e.data->xyz).length_sq() < vdwe * vdwe) {
+          keepDot = false;
+          break;
+        }
+      }
+    }
+    if (!keepDot) { continue; }
+
+    // We've gotten this far, so we score the dot and add it to the relevant subscore.
+
+    /// @todo
+
+
+    /// @todo Make sure that we add to the attraction subscore in the overlapType == 0 case
+  }
+
+  ret.bumpSubScore /= density;
+  ret.hBondSubScore /= density;
+  ret.attractSubScore /= density;
+  ret.valid = true;
+  return ret;
 }
 
 //===========================================================================================================
--- Scoring.h
+++ Scoring.h
@@ -3,12 +3,15 @@
 
 #pragma once
 
-#include "Common.h"
+#include "SpatialQuery.h"
 #include <cctbx/geometry_restraints/nonbonded.h>
 #include <iotbx/pdb/hierarchy.h>
 
 namespace molprobity {
   namespace probe {
+
+    //=====================================================================================================
+    // Helper functions outside the class.
 
     /// @brief Structure to hold the results from a call to closest_contact()
     typedef struct ContactResult_ {
@@ -25,37 +28,85 @@ namespace molprobity {
     ///       that it is inside the atom's surface.
     ContactResult closest_contact(Point dot, Point atom, double atom_radius);
 
+    /// @brief Return the signed-integer charge of the atom.
+    /// @param atom Atom whose charge is to be determined.
+    /// return Integer representing the charge: -2, -1, 0, 1, 2
+    int atom_charge(iotbx::pdb::hierarchy::atom const& atom);
+
+    //=====================================================================================================
+    /// @brief Class to hold data values for an atom beyond those present in the hierarchy::atom class itself
+    // that are needed by the Probe calculations.
+
+    class ExtraAtomInfo {
+    public:
+      double  vdwRadius = 0;            ///< van Der Waals radius of the atom
+      char    hydrogenBondType = ' ';   ///< Type of hydrogen bond for this atom
+
+      /// @todo We'll need to fill these in from somewhere to match the things that Reduce filled in from ElementInfo.cpp.
+      bool    isAcceptor = false;       ///< Does this accept hydrogen bonds (aromatic carbon, nitrogen acceptor,
+                                        ///  oxygen, sulfur, fluorine, chlorine, bromine, or iodine?
+      bool    isDonor = false;          ///< Is this a donor hydrogen (from polar, aromatic polar, or water)?
+      bool    isDummyHydrogen = false;  ///< These are inserted on Oxygens that are waters to provide
+                                        ///  bonds that can go in any direction.
+    };
+
     //=====================================================================================================
 
     /// @brief Class to handle scoring vectors of dots given two atoms.  Used by Reduce to compute energies.
     class AtomVsAtomDotScorer {
     public:
       /// @brief Constructor stores the non-bonded parameters to be used to determine atom features.
+      /// @param [in] extraInfo Vector of extra information pertaining to each atom in the structure
+      ///         that is being examined (the one that was used to construct the SpatialQuery
+      ///         structure).  Warning: The i_sel values from the atoms in the structure and the atoms
+      ///         passed as parameters are used to look up directly in this vector so they must not
+      ///         have changed (due to structure modification) since the extaInfo vector or the
+      ///         SpatialQuery structure were generated.
       /// @param [in] params Non-bonded parameters computed by the Geometry Restraints Manager.
       /// @param [in] gapWeight Factor to apply to gap between atoms
       /// @param [in] bumpWeight Factor to apply when atoms are in bumping overlap
       /// @param [in] hBondWeight Factor to apply to hydrogen-bond overlaps
-      AtomVsAtomDotScorer(cctbx::geometry_restraints::nonbonded_params const& params
+      /// @param [in] minRegularHydrogenBondGap How much overlap can there be between a hydrogen
+      ///             and the atom it is hydrogen-bonded to before we call it a clash when the atoms
+      ///             are not both charged.
+      /// @param [in] minChargedHydrogenBondGap How much overlap can there be between a hydrogen
+      ///             and the atom it is hydrogen-bonded to before we call it a clash when the
+      ///             atoms are both charged.
+      AtomVsAtomDotScorer(std::vector<ExtraAtomInfo> extraInfo
+        , cctbx::geometry_restraints::nonbonded_params const& params
         , double gapWeight = 0.25
         , double bumpWeight = 10.0
         , double hBondWeight = 4.0
-      ) : m_params(params), m_gapWeight(gapWeight), m_bumpWeight(bumpWeight), m_hBondWeight(hBondWeight) {};
+        , double minRegularHydrogenBondGap = 0.6
+        , double minChargedHydrogenBondGap = 0.8
+      ) : m_params(params), m_gapWeight(gapWeight), m_bumpWeight(bumpWeight), m_hBondWeight(hBondWeight)
+        , m_minRegularHydrogenBondGap(minRegularHydrogenBondGap)
+        , m_minChargedHydrogenBondGap(minChargedHydrogenBondGap) {};
 
       /// @brief Structure to hold the results from a call to score_dots()
       typedef struct ScoreDotsResult_ {
-        double  bumpSubScore;       ///< Portion of the score due to bumps
-        double  hBondSubScore;      ///< Portion of the score due to hydrogen bonds
-        double  totalScore;         ///< Sum of the bumpSubScore and the hBondSubScore, the total score
+        bool    valid = false;          ///< False if this information has not yet been computed.
+        double  bumpSubScore = 0;       ///< Portion of the score due to bumps
+        double  hBondSubScore = 0;      ///< Portion of the score due to hydrogen bonds
+        double  attractSubScore = 0;    ///< Portion of the score due to non-bumping attraction
+        /// @brief Sum of all of the sub-scores, the total score
+        double  totalScore() const { return bumpSubScore + hBondSubScore + attractSubScore; }
       } ScoreDotsResult;
 
-      /// @brief Determine the bump and hydrogen-bond subscores for a set of dots between two atoms
-      /// @param [in] sourceAtom Atom that the dots are surrounding
-      /// @param [in] targetAtom Atom to be tested against for bumping or hydrogen bonding.
+      /// @brief Determine the bump and hydrogen-bond subscores for a vector of dots on an atom
+      /// @param [in] sourceAtom Atom that the dots are surrounding.
+      /// @param [in] minOccupancy The minimum occupancy of the atom to be scored.
+      /// @param [in] spatialQuery Structure to ask for neighbors of the atom.
+      /// @param [in] nearbyRadius Maximum distance that an atom can be away and still be a neighbor
+      /// @param [in] probeRadius Radius of the probe rolled between the two potentially-contacting atoms
+      /// @param [in] excluded Atoms that are to be excluded from contact.
       /// @param [in] dots Vector of dots to compare.  Each is added to the sourceAtom origin.
-      /// @param [in] density Density of the dots on the probe sphere, used to normalize results
-      /// @param [in] onlyBumps If true, ignore near touches and count even hydrogen bonds as bumps
+      /// @param [in] density Density of the dots on the probe sphere, used to normalize results.
+      /// @param [in] onlyBumps If true, ignore near touches and count even hydrogen bonds as bumps.
       /// @return Normalized sum of scores, also broken down by hydrogen bond vs. bump scores.
-      ScoreDotsResult score_dots(iotbx::pdb::hierarchy::atom sourceAtom, iotbx::pdb::hierarchy::atom targetAtom,
+      ScoreDotsResult score_dots(iotbx::pdb::hierarchy::atom sourceAtom, double minOccupancy,
+        SpatialQuery spatialQuery, double nearbyRadius, double probeRadius,
+        std::vector<iotbx::pdb::hierarchy::atom> exclude,
         std::vector<Point> dots, double density, bool onlyBumps = false);
 
       //===========================================================================
@@ -67,10 +118,13 @@ namespace molprobity {
 
     protected:
       /// Parameters stored from constructor.
+      std::vector<ExtraAtomInfo> m_extraInfo;
       cctbx::geometry_restraints::nonbonded_params const& m_params;
       double m_gapWeight;
       double m_bumpWeight;
       double m_hBondWeight;
+      double m_minRegularHydrogenBondGap;
+      double m_minChargedHydrogenBondGap;
     };
 
     /// @todo Figure out what all of the things needed by Probe are.
