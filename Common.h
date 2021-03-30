4b188e81ce6369948e5fa3401a177edd4c7bf6b3
Russ Taylor <russ@reliasolve.com>
2021-03-30 17:25:03 -0400

    Converting from a typedef to a derived class to try and avoid runtime warning from Boost Python about a multiply-registered class.

--- Common.h
+++ Common.h
@@ -12,8 +12,18 @@ namespace molprobity {
     /// The type to be used for a single coordinate in space
     typedef double Coord;
 
-    /// A location in space
-    typedef scitbx::vec3<Coord> Point;
+    /// @brief A location in space
+    ///
+    /// This is a class rather than a typedef because the typedef confused
+    /// Boost Python when we tried provide an element accessor to it; it claimed
+    /// that we had already registered a to-Python converter, but when we did
+    /// not add one it failed because none had been defined...
+    class Point : public scitbx::vec3<Coord> {
+    public:
+      Point(scitbx::vec3<Coord> const &c) : scitbx::vec3<Coord>(c[0],c[1],c[2]) { }
+      Point(Coord x, Coord y, Coord z) : scitbx::vec3<Coord>(x,y,z) { }
+      Point() : scitbx::vec3<Coord>() { }
+    };
 
   }
 }
